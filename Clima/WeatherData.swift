400ff94799251ad7eb63c4d567c221a09ed77901
Tehillahk <kangambt@myumanitoba.ca>
2025-01-26 04:14:52 -0600

    Added struct for decoding json

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,43 @@
+//
+//  Weather.swift
+//  Clima
+//
+//  Created by Tehillah Kangamba on 2025-01-26.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+   
+}
+
+struct Clouds: Codable {
+    let all: Int
+}
+
+struct Main: Codable {
+    let temp: Double
+   
+}
+
+
+struct Sys: Codable {
+    let country: String
+    let sunrise, sunset: Int
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
--- Clima/WeatherLogic.swift
+++ Clima/WeatherLogic.swift
@@ -12,11 +12,42 @@ struct WeatherLogic{
     var city = ""
     let API_KEY = env["OPEN_WEATHER_API_KEY"] ?? "failed"
 
-    mutating func getWeatherInfo(city: String){
-        self.city = city
+    func getWeatherInfo(city: String){
+      //  self.city = city
         let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(API_KEY)"
         
-        print(apiUrl)
+        let url = URL(string: apiUrl)
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url! ,completionHandler: {
+            (data: Data?, response:URLResponse?, error:Error? ) in
+            if(error != nil){
+                print(error!)
+                return
+            }
+            
+            if let safeData = data{
+               // let dataString = String(data: safeData, encoding: .utf8)
+                self.parseJson(safeData)
+            }
+        })
+        
+        task.resume()
+        
+        
     }
     
+    func parseJson(_ jsonString: Data){
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: jsonString)
+            print("temp\(decodedData.main.temp)")
+        }catch{
+            print(error)
+        }
+        
+        
+        
+    }
 }
