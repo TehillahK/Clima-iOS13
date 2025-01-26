8c771e2d2a5436e92b5f64e5381685a1f92d5345
Tehillahk <kangambt@myumanitoba.ca>
2025-01-26 00:21:53 -0600

    added delegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -17,8 +18,25 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.searchTextField.delegate = self
     }
 
-
+    @IBAction func searchBtnPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    // runs if they press go or ok in on screen keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
 }
 
